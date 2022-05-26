b9ef5697a4d0054d4048b606dafe421405f5a025
James Robertson <digital.robertson@gmail.com>
2022-05-26 16:55:05 +0100

    feature: Improved robustness using Nokorexi.

--- ferrexi.gemspec
+++ ferrexi.gemspec
@@ -1,14 +1,14 @@
 Gem::Specification.new do |s|
   s.name = 'ferrexi'
-  s.version = '0.1.2'
+  s.version = '0.2.0'
   s.summary = 'A wrapper over the Ferrum gem to return a Rexle document.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/ferrexi.rb']
   s.add_runtime_dependency('ferrum', '~> 0.7', '>=0.7')
-  s.add_runtime_dependency('rexle', '~> 1.5', '>=1.5.4')
+  s.add_runtime_dependency('nokorexi', '~> 0.7', '>=0.7.0')
   s.signing_key = '../privatekeys/ferrexi.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/ferrexi'
 end
--- lib/ferrexi.rb
+++ lib/ferrexi.rb
@@ -3,26 +3,26 @@
 # file: ferrexi.rb
 
 require 'ferrum'
-require 'rexle'
+require 'nokorexi'
 
 
 class Ferrexi
 
   attr_reader :to_doc
 
-  def initialize(url, debug: false)
-    
+  def initialize(url, wait: 0, debug: false)
+
     browser = Ferrum::Browser.new
     browser.goto(url)
-    
+
     # without the following statement the DOM probably won't have loaded yet.
     # As a result, falls back to returning the original document source
-    
-    browser.network.wait_for_idle 
-    
-    @to_doc = Rexle.new(browser.body, debug: debug)
-    
-  end
-  
-end
 
+    browser.network.wait_for_idle
+    sleep wait
+
+    @to_doc = Nokorexi.new(browser.body).to_doc
+
+  end
+
+end
